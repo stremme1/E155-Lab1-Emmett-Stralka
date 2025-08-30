61a3de5c3f903523156c2bd868413c16444bfca0
stremme1 <132714265+stremme1@users.noreply.github.com>
2025-08-29 20:46:33 -0700

    Update seven_segment.sv

--- E155- Emmett_Stralka/fpga/src/seven_segment.sv
+++ E155- Emmett_Stralka/fpga/src/seven_segment.sv
@@ -18,19 +18,27 @@ module seven_segment (
 // 8 = A,B,C,D,E,F,G
 // 9 = A,B,C,D,F,G
 
+
     always_comb begin
         unique case (num)
-            4'd0: seg = 7'b1000000; // A,B,C,D,E,F ON; G OFF
-            4'd1: seg = 7'b1111001; // B,C ON
-            4'd2: seg = 7'b0100100; // A,B,G,E,D
-            4'd3: seg = 7'b0110000; // A,B,C,G,D
-            4'd4: seg = 7'b0011001; // B,C,F,G
-            4'd5: seg = 7'b0010010; // A,C,D,F,G
-            4'd6: seg = 7'b0000010; // A,C,D,E,F,G
-            4'd7: seg = 7'b1111000; // A,B,C
-            4'd8: seg = 7'b0000000; // all on
-            4'd9: seg = 7'b0010000; // A,B,C,D,F,G
-            default: seg = 7'b1111111; // blank (all OFF)
+            4'd0: seg = 7'b1000000; // 0: A,B,C,D,E,F ON; G OFF
+            4'd1: seg = 7'b1111001; // 1: B,C
+            4'd2: seg = 7'b0100100; // 2: A,B,G,E,D
+            4'd3: seg = 7'b0110000; // 3: A,B,C,G,D
+            4'd4: seg = 7'b0011001; // 4: B,C,F,G
+            4'd5: seg = 7'b0010010; // 5: A,C,D,F,G
+            4'd6: seg = 7'b0000010; // 6: A,C,D,E,F,G
+            4'd7: seg = 7'b1111000; // 7: A,B,C
+            4'd8: seg = 7'b0000000; // 8: all segments ON
+            4'd9: seg = 7'b0010000; // 9: A,B,C,D,F,G
+            4'd10: seg = 7'b0001000; // A: A,B,C,E,F,G
+            4'd11: seg = 7'b0000011; // B: C,D,E,F,G
+            4'd12: seg = 7'b1000110; // C: A,D,E,F
+            4'd13: seg = 7'b0100001; // D: B,C,D,E,G
+            4'd14: seg = 7'b0000110; // E: A,D,E,F,G
+            4'd15: seg = 7'b0001110; // F: A,E,F,G
+            default: seg = 7'b1111111; // blank
         endcase
     end
+
 endmodule
